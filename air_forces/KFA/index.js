524192679ae4e7bcf2fd5797953eb2699580b7d9
Kitten Films <kittenapps@icloud.com>
2025-02-25 11:01:43 +0500

    Update index.js

--- air_forces/KFA/index.js
+++ air_forces/KFA/index.js
@@ -1,4 +1,13 @@
 var content = "It worked!"
+var one = "one"
 
+var url_string = window.location.href; 
+var url = new URL(url_string);
+var file = url.searchParams.get("f");
 var doc = document.getElementById("content");
-doc.innerHTML = content
+
+if (file == "one") {
+  doc.innerHTML = one
+} else {
+  doc.innerHTML = content
+}
