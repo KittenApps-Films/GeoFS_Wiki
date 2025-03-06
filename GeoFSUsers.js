05e06463e44e24fb97b7592950e166143f90d261
Adding user buzz to users addon <kittenappsandfilms@gmail.com>
2025-03-06 06:49:33 +0500

    added to GeoFSUsers.js

--- GeoFSUsers.js
+++ GeoFSUsers.js
@@ -1,2 +1,2 @@
 
-export var users = {"KittenFilms[KFA]":"kittenapps-films.github.io/geofs/kfa/","buzz":"google.com","buzzer2":"youtube.com","player":"geo-fs.com"}
+export var users = {"KittenFilms[KFA]":"kittenapps-films.github.io/geofs/kfa/","buzz":"brick.com","buzzer2":"youtube.com","player":"geo-fs.com"}
