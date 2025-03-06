95a6098a2ac69f387e3136f630a377a2d6a870d1
Adding user player to users addon <kittenappsandfilms@gmail.com>
2025-03-06 06:42:35 +0500

    added to GeoFSUsers.js

--- GeoFSUsers.js
+++ GeoFSUsers.js
@@ -1,2 +1,2 @@
 
-export var users = {"KittenFilms[KFA]":"kittenapps-films.github.io/geofs/kfa/","buzz":"google.","buzzer2":"youtube.com"}
+export var users = {"KittenFilms[KFA]":"kittenapps-films.github.io/geofs/kfa/","buzz":"google.","buzzer2":"youtube.com","player":"geo-fs.com"}
